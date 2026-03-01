69f01f991a13f4a916f7c113bb700297a4c1b57c
Dane <danegiano@gmail.com>
2026-03-01 11:46:07 +0100

    feat: add combat sound effects (hit, kill, hurt)

--- main.py
+++ main.py
@@ -769,14 +769,24 @@ def draw_victory(screen):
 
 
 def handle_combat(player, enemies, boss=None):
+    hit_events = []
+
     # Player attack hits enemies
     if player.attacking and player.attack_rect:
         for enemy in enemies:
             if enemy.alive and player.attack_rect.colliderect(enemy.rect):
                 enemy.take_damage(1)
+                if not enemy.alive:
+                    hit_events.append("enemy_die")
+                else:
+                    hit_events.append("hit_enemy")
         # Player attack hits boss
         if boss and boss.alive and player.attack_rect.colliderect(boss.rect):
             boss.take_damage(1)
+            if not boss.alive:
+                hit_events.append("boss_die")
+            else:
+                hit_events.append("hit_enemy")
 
     # Enemies damage player on contact
     for enemy in enemies:
@@ -788,6 +798,7 @@ def handle_combat(player, enemies, boss=None):
                 else:
                     player.rect.x += 30
                 player.vel_y = -8  # Small bounce up
+                hit_events.append("player_hurt")
 
     # Boss damages player
     if boss and boss.alive:
@@ -798,11 +809,15 @@ def handle_combat(player, enemies, boss=None):
                 else:
                     player.rect.x += 40
                 player.vel_y = -10
+                hit_events.append("player_hurt")
         # Shockwave damages player
         if boss.shockwave and boss.shockwave_timer > 0:
             if player.rect.colliderect(boss.shockwave):
                 if player.take_damage(1):
                     player.vel_y = -12
+                    hit_events.append("player_hurt")
+
+    return hit_events
 
 
 def draw_boss_hp(screen, boss):
@@ -928,7 +943,10 @@ def main():
                 if boss.summoned_flies:
                     enemies.extend(boss.summoned_flies)
                     boss.summoned_flies = []
-            handle_combat(player, enemies, boss)
+            combat_events = handle_combat(player, enemies, boss)
+            for evt in combat_events:
+                if evt in sounds:
+                    sounds[evt].play()
             camera.update(player)
 
             # Check for death
